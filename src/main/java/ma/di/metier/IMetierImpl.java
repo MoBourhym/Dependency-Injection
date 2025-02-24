9abb2f9471a2c113810573b3a8dd2ef468064e41
BOURHYM <mohammedbourhym9@gmail.com>
2025-02-24 09:53:53 +0000

    Q4 IMetier Interface implementation with  public IMetierImpl(IDao dao) {

---
+++ src/main/java/ma/di/metier/IMetierImpl.java
@@ -0,0 +1,30 @@
+package ma.di.metier;
+
+import ma.di.dao.IDao;
+
+public class IMetierImpl implements IMetier{
+    private IDao dao;
+    @Override
+    public double calcul() {
+        double data= dao.getData();
+        double result=data*data;
+        return 0;
+    }
+
+    public IMetierImpl() {
+
+    }
+
+
+    public IMetierImpl(IDao dao) {
+        this.dao = dao;
+    }
+
+    public IDao getDao() {
+        return dao;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
